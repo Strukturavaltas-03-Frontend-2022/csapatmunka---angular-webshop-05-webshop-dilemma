b9b86290b750c62ce99872e2342bfda4794a0c13
szabi1112 <banki.szabolcs@gmail.com>
2022-11-17 19:41:01 +0100

    Kategória kutyuska is működik

--- src/app/page/cat01/cat01.component.ts
+++ src/app/page/cat01/cat01.component.ts
@@ -8,9 +8,56 @@ import { ProductService } from 'src/app/service/product.service';
 })
 export class Cat01Component {
   private categoryId = 1;
-  private products = this.productservice.getProducts();
-  public productsFeatured = this.products.filter(product=> product.catId === this.categoryId && product.featured);
-  public productsActive = this.products.filter(product=> product.catId === this.categoryId);
+  private products = this.productservice.getProducts()
+  public productsActive = this.products.filter(product => product.catId === this.categoryId);
+  public productsDiscounted = this.productsActive.filter(product =>  product.discounted);
+  public productsFeatured = this.productsActive.filter(product=> product.featured);
+
 
   constructor(private productservice: ProductService) {}
+
+  toSort(id:string):void{
+    if(id === 'nameAZ' ){
+      this.productservice.sortAZ(this.productsActive)
+        this.productservice.productsChanged.subscribe(
+        productList => this.productsActive = productList
+    )}
+
+    if(id === 'nameZA' ){
+      this.productservice.sortZA(this.productsActive)
+        this.productservice.productsChanged.subscribe(
+        productList => this.productsActive = productList
+    )}
+
+    if(id === 'price19' ){
+      this.productservice.sort19(this.productsActive)
+        this.productservice.productsChanged.subscribe(
+        productList => this.productsActive = productList
+    )}
+
+    if(id === 'price91' ){
+      this.productservice.sort91(this.productsActive)
+        this.productservice.productsChanged.subscribe(
+        productList => this.productsActive = productList
+    )}
+
+    if(id === 'discount' ){
+      this.productservice.getDiscounted(this.productsActive)
+        this.productservice.productsChanged.subscribe(
+        productList => this.productsActive = this.productsDiscounted
+    )}
+
+    if(id === 'featured' ){
+      this.productservice.getFeatured(this.productsActive)
+        this.productservice.productsChanged.subscribe(
+        productList => this.productsActive = this.productsFeatured
+    )}
+
+  }
+
+  ngOnInit(){
+    this.productservice.productsChanged.subscribe(
+      productList => this.productsActive = productList
+    )
+  }
 }
